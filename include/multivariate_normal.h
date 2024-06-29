6001533d753913f684034050014d103d46063f08
Foxp1 <giardina.fa@gmail.com>
2024-06-29 10:43:12 +0200

    Add conditional distribution for multivariate normal vector

--- include/multivariate_normal.h
+++ include/multivariate_normal.h
@@ -2,45 +2,78 @@
 
 #include "random_vector.h"
 
-namespace shrew {
-namespace random_vector {
+#include <iostream>
+#include <numeric>
 
-/// @brief Multivariate nomal distribution as a random vector
-/// @tparam n 
-template <int n>
-class MultivariateNormal : public RandomVector<n> {
- public:
-  virtual double joint_pdf(Eigen::Matrix<double, n, 1> x) const override;
+namespace shrew
+{
+  namespace random_vector
+  {
 
-  MultivariateNormal(Eigen::Matrix<double, n, 1> mu, Eigen::Matrix<double, n, n> K) : K(K), mu(mu) 
+    /// @brief Multivariate nomal distribution as a random vector
+    /// @tparam n
+    template <int n>
+    class MultivariateNormal : public RandomVector<n>
     {
-      det_K = K.determinant();
-      K_inv = K.inverse();
+    public:
+      virtual double joint_pdf(Eigen::Matrix<double, n, 1> x) const override;
+
+      MultivariateNormal(Eigen::Matrix<double, n, 1> mu, Eigen::Matrix<double, n, n> K) : K(K), mu(mu)
+      {
+        det_K = K.determinant();
+        K_inv = K.inverse();
+      };
+
+      const Eigen::Matrix<double, n, n> K;
+      const Eigen::Matrix<double, n, 1> mu;
+
+    private:
+      double det_K;
+      Eigen::Matrix<double, n, n> K_inv;
     };
 
-  const Eigen::Matrix<double, n, n> K;
-  const Eigen::Matrix<double, n, 1> mu;
+    template <int n>
+    double MultivariateNormal<n>::joint_pdf(Eigen::Matrix<double, n, 1> x) const
+    {
+      return pow(2 * M_PI, -n / 2.0) / sqrt(det_K) * exp(-0.5 * (x - mu).transpose() * K_inv * (x - mu));
+    }
 
- private:
-  double det_K;
-  Eigen::Matrix<double, n, n> K_inv;
+    template <int n, int m>
+    MultivariateNormal<m> getMarginal(MultivariateNormal<n> random_vector, std::vector<int> marginal_indices)
+    {
+      return MultivariateNormal<m>(random_vector.mu(marginal_indices), random_vector.K(marginal_indices, marginal_indices));
+    }
 
-};
+    template <int n, int m>
+    MultivariateNormal<m> getConditional(MultivariateNormal<n> random_vector, std::vector<int> conditional_indices, char _operator, Eigen::Matrix<double, m, 1> value)
+    {
+      std::vector<int> non_conditional_indices(n - conditional_indices.size());
+      for (int i = 0, k = 0; i < n - conditional_indices.size(); ++i)
+        if (i == conditional_indices[k])
+          k += 1;
+        else
+          non_conditional_indices[i] = i;
 
-template<int n>
-double MultivariateNormal<n>::joint_pdf(Eigen::Matrix<double, n, 1> x) const {
-  return pow(2 * M_PI, -n/2.0) / sqrt(det_K) * exp(-0.5 * (x - mu).transpose() * K_inv * (x - mu));
-}
+      Eigen::Matrix<double, m, 1> mu_conditioned;
+      Eigen::Matrix<double, m, m> K_conditioned;
+      if (_operator == '=')
+      {
+        auto mu_1 = random_vector.mu(non_conditional_indices);
+        auto mu_2 = random_vector.mu(conditional_indices);
+        auto sigma_11 = random_vector.K(non_conditional_indices, non_conditional_indices);
+        auto sigma_12 = random_vector.K(non_conditional_indices, conditional_indices);
+        auto sigma_22_inv = random_vector.K(conditional_indices, conditional_indices).inverse();
 
-template<int n, int m>
-MultivariateNormal<m> getMarginal(MultivariateNormal<n> random_vector, std::vector<int> marginal_indices) {
-  return MultivariateNormal<m>(random_vector.mu(marginal_indices), random_vector.K(marginal_indices, marginal_indices));
-}
+        mu_conditioned = mu_1 + sigma_12 * sigma_22_inv * (value - mu_2);
+        K_conditioned = sigma_11 - sigma_12 * sigma_22_inv * sigma_12.transpose();
+      }
+      else
+      {
+        throw std::logic_error("Error: Requested conditional operator not implemented for multivariate normal distribution.");
+      }
 
-template<int n, int m>
-MultivariateNormal<m> getConditional(std::vector<int> conditioned_indices[], char _operator, std::vector<double> value) {
-  return new MultivariateNormal<m>();
-}
+      return MultivariateNormal<m>(mu_conditioned, K_conditioned);
+    }
 
-}  // namespace random_variable
-}  // namespace shrew
+  } // namespace random_variable
+} // namespace shrew
--- tests/multivariate_normal.cpp
+++ tests/multivariate_normal.cpp
@@ -33,11 +33,23 @@ TEST_F(MultivariateNormalTestFixture, TrivariateNormalTest) {
 TEST_F(MultivariateNormalTestFixture, MarginalNormalTest) {
   auto mu = Eigen::Matrix<double, 3, 1> {0.5, -0.2, 0.1};
   auto K = Eigen::Matrix<double, 3, 3> {{2.0, 0.3, 0.5}, {0.3, 1.0, 0.6}, {0.5, 0.6, 1.3}};
-  auto mvec = MultivariateNormal<3>(mu, K);
   auto marginal = getMarginal<3,2>(MultivariateNormal<3>(mu, K), std::vector<int>{ 0, 2 });
 
   auto K_confirm = Eigen::Matrix<double, 2, 2> {{2.0, 0.5}, {0.5, 1.3}};
   auto mu_confirm = Eigen::Matrix<double, 2, 1> {0.5, 0.1};
   auto x_confirm = Eigen::Vector2d(1.0, -1.0);
   ASSERT_EQ(marginal.joint_pdf(x_confirm), MultivariateNormal<2>(mu_confirm, K_confirm).joint_pdf(x_confirm));
+}
+
+TEST_F(MultivariateNormalTestFixture, ConditionalNormalWithEqualityTest) {
+  auto mu = Eigen::Matrix<double, 4, 1> {1, 2, 3, 4};
+  auto K = Eigen::Matrix<double, 4, 4> {{4.0, 1.0, 0.5, 0.2}, {1.0, 3.0, 0.3, 0.4}, {0.5, 0.3, 2.0, 0.1}, {0.2, 0.4, 0.1, 1.0}};
+  auto conditional = getConditional<4,2>(MultivariateNormal<4>(mu, K), std::vector<int>{ 2, 3 }, '=', Eigen::Matrix<double, 2, 1>{ 5.0, 6.0 });
+
+  ASSERT_NEAR(conditional.mu(0), 1.83417085427136, 1e-14);
+  ASSERT_NEAR(conditional.mu(1), 3.03517587939699, 1e-14);
+  ASSERT_NEAR(conditional.K(0, 0), 3.84422110552764, 1e-14);
+  ASSERT_NEAR(conditional.K(1, 0), 0.857286432160804, 1e-14);
+  ASSERT_NEAR(conditional.K(0, 1), 0.857286432160804, 1e-14);
+  ASSERT_NEAR(conditional.K(1, 1), 2.80603015075377, 1e-14);
 }
