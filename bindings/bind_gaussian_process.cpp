ef8971a80e423e9acc02bd65a4144ef82dc57f48
Fabio Giardina <56641026+ffgiardina@users.noreply.github.com>
2025-05-31 09:28:16 +0200

    Update python bindings (#7)

--- bindings/bind_gaussian_process.cpp
+++ bindings/bind_gaussian_process.cpp
@@ -14,38 +14,82 @@ using namespace gaussian_process;
 using namespace gaussian_process::kernel;
 
 void bind_gaussian_process(py::module_ &m) {
+    py::class_<HpOptimizationMeta>(m, "HpOptimizationMeta")
+        .def(py::init<>())
+        .def_readwrite("iter", &HpOptimizationMeta::iter)
+        .def_readwrite("running_max", &HpOptimizationMeta::running_max)
+        .def_readwrite("optim_progress_intervall", &HpOptimizationMeta::optim_progress_intervall)
+        .def_readwrite("progress_output", &HpOptimizationMeta::progress_output)
+        .def_readwrite("algo", &HpOptimizationMeta::algo)
+        .def_readwrite("y_", &HpOptimizationMeta::y_)
+        .def_readwrite("x_", &HpOptimizationMeta::x_)
+        .def_readwrite("kernel_", &HpOptimizationMeta::kernel_);
+
     py::class_<GaussianProcess>(m, "GaussianProcess")
         .def(py::init<Eigen::VectorXd , Eigen::VectorXd, Eigen::VectorXi, Kernel&>(), py::arg("x"), py::arg("y"), py::arg("conditional_indices"), py::arg("kernel"))
         .def("log_marginal_likelihood", py::overload_cast<>(&GaussianProcess::LogMarginalLikelihood))
         .def("optimize", &GaussianProcess::OptimizeHyperparameters, py::arg("progress_output") = true)
-        .def("get_posterior", &GaussianProcess::GetPosteriorGP);
+        .def("get_posterior", &GaussianProcess::GetPosteriorGP)
+        .def_readwrite("x", &GaussianProcess::x)
+        .def_readwrite("y", &GaussianProcess::y)
+        .def_readwrite("conditional_indices", &GaussianProcess::conditional_indices)
+        .def_readwrite("kernel", &GaussianProcess::kernel)
+        .def_readwrite("hp_opt_meta", &GaussianProcess::hp_opt_meta);
 
     py::class_<Kernel>(m, "Kernel");
 
     py::class_<Hyperparameters, std::shared_ptr<Hyperparameters>>(m, "Hyperparameters");
 
     py::class_<SEHyperparams, Hyperparameters, std::shared_ptr<SEHyperparams>>(m, "SEHyperparams")
-        .def(py::init<double, double, double>(), py::arg("signal_stdv"), py::arg("lengthscale"), py::arg("noise_stdv"));
+        .def(py::init<double, double, double>(), py::arg("signal_stdv"), py::arg("lengthscale"), py::arg("noise_stdv"))
+        .def_readwrite("signal_stdv", &SEHyperparams::signal_stdv)
+        .def_readwrite("lengthscale", &SEHyperparams::lengthscale)
+        .def_readwrite("noise_stdv", &SEHyperparams::noise_stdv);
 
     py::class_<MaternHyperparams, Hyperparameters, std::shared_ptr<MaternHyperparams>>(m, "MaternHyperparams")
         .def(py::init<double, double, double, MaternSmoothness>(), 
-        py::arg("signal_stdv"), py::arg("lengthscale"), py::arg("noise_stdv"), py::arg("nu"));
+        py::arg("signal_stdv"), py::arg("lengthscale"), py::arg("noise_stdv"), py::arg("nu"))
+        .def_readwrite("signal_stdv", &MaternHyperparams::signal_stdv)
+        .def_readwrite("lengthscale", &MaternHyperparams::lengthscale)
+        .def_readwrite("noise_stdv", &MaternHyperparams::noise_stdv)
+        .def_readwrite("nu", &MaternHyperparams::nu);
 
     py::class_<MaternExtendedHyperparams, MaternHyperparams, std::shared_ptr<MaternExtendedHyperparams>>(m, "MaternExtendedHyperparams")
         .def(py::init<double, double, double, MaternSmoothness, std::vector<double>>(), 
-        py::arg("signal_stdv"), py::arg("lengthscale"), py::arg("noise_stdv"), py::arg("nu"), py::arg("override_noise_stdv"));
+        py::arg("signal_stdv"), py::arg("lengthscale"), py::arg("noise_stdv"), py::arg("nu"), py::arg("override_noise_stdv"))
+        .def_readwrite("override_noise_stdv", &MaternExtendedHyperparams::override_noise_stdv);
 
     py::class_<MaternExtended, Kernel>(m, "MaternExtended")
         .def(py::init<std::shared_ptr<Hyperparameters>, std::shared_ptr<Hyperparameters>, std::shared_ptr<Hyperparameters>, Eigen::VectorXi, Eigen::VectorXi>(), py::arg("hyperparameters"), py::arg("lower_bounds"), py::arg("upper_bounds"), py::arg("conditional_indices"), py::arg("override_conditional_indices"))
-        .def("get_hyperparameters", &MaternExtended::GetHyperparameters);
+        .def("get_hyperparameters", &MaternExtended::GetHyperparameters)
+        .def("get_hp_lower_bounds", &MaternExtended::GetHpLowerBounds)
+        .def("get_hp_upper_bounds", &MaternExtended::GetHpUpperBounds)
+        .def("set_hyperparameters", &MaternExtended::SetHyperparameters)
+        .def("set_hp_lower_bounds", &MaternExtended::SetHpLowerBounds)
+        .def("set_hp_upper_bounds", &MaternExtended::SetHpUpperBounds)
+        .def("get_conditional_indices", &MaternExtended::GetConditionalIndices)
+        .def("set_conditional_indices", &MaternExtended::SetConditionalIndices);
 
     py::class_<Matern, Kernel>(m, "Matern")
         .def(py::init<std::shared_ptr<Hyperparameters>, std::shared_ptr<Hyperparameters>, std::shared_ptr<Hyperparameters>, Eigen::VectorXi>(), py::arg("hyperparameters"), py::arg("lower_bounds"), py::arg("upper_bounds"), py::arg("conditional_indices"))
-        .def("get_hyperparameters", &Matern::GetHyperparameters);
+        .def("get_hyperparameters", &Matern::GetHyperparameters)
+        .def("get_hp_lower_bounds", &Matern::GetHpLowerBounds)
+        .def("get_hp_upper_bounds", &Matern::GetHpUpperBounds)
+        .def("set_hyperparameters", &Matern::SetHyperparameters)
+        .def("set_hp_lower_bounds", &Matern::SetHpLowerBounds)
+        .def("set_hp_upper_bounds", &Matern::SetHpUpperBounds)
+        .def("get_conditional_indices", &Matern::GetConditionalIndices);
 
     py::class_<SquaredExponential, Kernel>(m, "SquaredExponential")
         .def(py::init<std::shared_ptr<Hyperparameters>, std::shared_ptr<Hyperparameters>, std::shared_ptr<Hyperparameters>, Eigen::VectorXi>(), py::arg("hyperparameters"), py::arg("lower_bounds"), py::arg("upper_bounds"), py::arg("conditional_indices"))
-        .def("get_hyperparameters", &SquaredExponential::GetHyperparameters);
+        .def("get_hyperparameters", &SquaredExponential::GetHyperparameters)
+        .def("get_hp_lower_bounds", &SquaredExponential::GetHpLowerBounds)
+        .def("get_hp_upper_bounds", &SquaredExponential::GetHpUpperBounds)
+        .def("set_hyperparameters", &SquaredExponential::SetHyperparameters)
+        .def("set_hp_lower_bounds", &SquaredExponential::SetHpLowerBounds)
+        .def("set_hp_upper_bounds", &SquaredExponential::SetHpUpperBounds)
+        .def("get_conditional_indices", &SquaredExponential::GetConditionalIndices)
+        .def("set_conditional_indices", &SquaredExponential::SetConditionalIndices);
 
     py::enum_<MaternSmoothness>(m, "MaternSmoothness")
         .value("NU_0_5", MaternSmoothness::NU_0_5)
